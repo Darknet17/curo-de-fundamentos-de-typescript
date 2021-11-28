c089e6a7530c8e92c3b4a6b9238fa72cb77d5122
Luis Josue Santana <josuesantana17@gmail.com>
2021-11-28 19:42:47 -0400

    Number, Boolean y String

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,39 @@
 "use strict";
 console.log("Hola Typescript con Platzi!");
+// Tipo: Number
+// Explicito
+let phone;
+phone = 1;
+phone = 1234567890;
+// phone = "Hola"; // Error
+// Inferido
+let phoneNumber = 1234567890;
+phoneNumber = 123;
+// phone = true; // Error por tipo de dato
+let hex = 0xf00d;
+let binary = 0b1010;
+let octal = 0o744;
+// Tipo: Boolean
+// Tipado Explicito
+let isPro;
+isPro = true;
+// isPro = 1; // Error
+// Inferido
+let isUserPro = false;
+isUserPro = true;
+// isUserPro = 10; // Error
+// Tipo: String
+let username = "Luis Josue";
+username = "Luis Jose";
+// username = true; // Error por el tipo de dato
+// Template String
+// Uso de Back-Tick `
+let userInfo;
+userInfo = `
+    User Info:
+    username: ${username}
+    firstName: ${username + ' Santana'}
+    phone: ${phone}
+    isPro: ${isPro}
+`;
+console.log(userInfo);
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,47 @@
 console.log("Hola Typescript con Platzi!");
+
+// Tipo: Number
+// Explicito
+let phone: number;
+phone = 1;
+phone = 1234567890;
+// phone = "Hola"; // Error
+
+// Inferido
+let phoneNumber = 1234567890;
+phoneNumber = 123;
+// phone = true; // Error por tipo de dato
+
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+// Tipo: Boolean
+// Tipado Explicito
+let isPro: boolean;
+isPro = true;
+// isPro = 1; // Error
+
+// Inferido
+
+let isUserPro = false;
+isUserPro = true;
+// isUserPro = 10; // Error
+
+// Tipo: String
+let username: string = "Luis Josue";
+username = "Luis Jose";
+// username = true; // Error por el tipo de dato
+
+// Template String
+// Uso de Back-Tick `
+let userInfo: string;
+
+userInfo = `
+    User Info:
+    username: ${username}
+    firstName: ${ username + ' Santana' }
+    phone: ${phone}
+    isPro: ${isPro}
+`;
+console.log(userInfo)
