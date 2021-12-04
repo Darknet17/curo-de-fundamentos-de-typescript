3a473196ee2b78ce25b19580c540fe7f8b5810be
Luis Josue Santana <josuesantana17@gmail.com>
2021-12-03 22:06:57 -0400

    function

--- dist/function.js
+++ dist/function.js
@@ -1,53 +1,44 @@
+"use strict";
 // Crear una Fotografia
-
-// function createPicture(title, date, size) {
-//     //title
-// }
-type SquareSize = '100x100' | '500x500' | '1000x1000';
 // Usamos TS, definimos tipos para parametros
 // function createPicture(title: string, date:string, size: SquareSize) {
 //     console.log('create Picture using', title, date, size);
 // }
-
 // createPicture('My Birthday', '2021-12-3', '1000x1000');
 // createPicture('My Birthday', '2021-12-3');
-
 // parametros opciones en funciones
-function createPicture(title?: string, date?:string, size?: SquareSize) {
+function createPicture(title, date, size) {
     console.log('create Picture using', title, date, size);
 }
-
 // createPicture('My Birthday', '2021-12-3', '1000x1000');
 // createPicture('My Birthday', '2021-12-3');
 // createPicture('My Birthday');
 // createPicture();
-
 // Flat Array function
-let createPic = (title: string, data: string, size: SquareSize): object => {
+let createPic = (title, data, size) => {
     return {
         title,
         data,
         size
-    }
-}
-
+    };
+};
 const picture = createPic('My Birthday', '2021-12-3', '1000x1000');
 console.log('picture', picture);
-
 // Tipo de retorno con TypeScript
-function handleError(code: number, message: string): never | string {
+function handleError(code, message) {
     // Procesamiento del codigo, mensaje
-    if ( message === 'error'){
+    if (message === 'error') {
         throw new Error(`${message}. Code error: ${code}`);
-    } else {
+    }
+    else {
         return 'An error has occured';
     }
 }
-
 try {
     let result = handleError(200, 'OK'); // string
     console.log(result);
     result = handleError(404, 'error'); // Never
     console.log(result);
-} catch (e) {
+}
+catch (e) {
 }
